3e789a1e1d4d59c47177288cad58a9db5fa1927f
Crypto <augustine.wittkower@gmail.com>
2014-03-20 09:58:22 -0700

    custom auth in progress

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,32 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  helper_method :current_user, :logged_in?
+
+  def current_user
+    @current_user ||= User.find_by(token: session[:token])
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  def login(user)
+    @current_user = user
+    session[:token] = user.reset_token!
+  end
+
+  def logout
+    curent_user.try(:reset_token!)
+    session[:token]  = nil
+  end
+
+  def require_logged_in!
+    redirect_to new_session_url unless logged_in?
+  end
+
+  def require_logged_out!
+    redirect_to new_session_url if logged_in?
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,41 @@
 class User < ActiveRecord::Base
+  attr_reader :password
+  
+  validates :username, :species, uniqueness: true, presence: true
+  validates :password_digest, presence: true
+  validates :password, length: { minimum: 6, allow_nil: true }
+  validates :token, presence: true, uniqueness: true
+
+  before_validation :ensure_session_token
+
+  def self.find_by_credentials(username, password)
+    user = User.find_by_username(username)
+    user.try(:is_password?, password) ? user : nil
+  end
+
+  def self.generate_token
+    SecureRandom::urlsafe_base64(16)
+  end
+
+  def is_password?(secret)
+    BCrypt::Password.new(self.password_digest).is_password(secret)
+  end
+
+  def password=(secret)
+    if(secret).present?
+      @password = secret
+      self.password_digest = BCrypt::Password.create(secret)
+    end
+  end
+
+  def reset_token!
+    self.token = self.class.generate_token
+    self.save!
+    self.token
+  end
+
+  private
+  def ensure_session_token
+    self.token ||= self.class.manifest_token
+  end
 end
